a6202fbd27ce9fdc32c6a8401127223bdb1d3d6a
niklas <nyholmniklas@gmail.com>
2017-04-04 23:29:35 +0300

    Omit cards with no valid img

--- js/util/QueryUtils.js
+++ js/util/QueryUtils.js
@@ -57,19 +57,26 @@ class QueryUtils {
     /**
      * Takes an array of cards and and returns a filtered array of cards without duplicates of the
      * same card from different editions/sets. Additionally we try to pick the correct image for that card.
+     * Note that if an image cannot be found, the card will be omitted altogether from the array this function returns.
+     *
      */
     static _setCardImageUrls(cards) {
+        let filteredCards = [];
         for (var i = 0; i < cards.length; i++) {
-            var card = cards[i];
+            let card = cards[i];
+            let imageFound = false;
             for (var k = 0; k < card.editions.length; k++) {
                 if (card.editions[k].image_url != "https://image.deckbrew.com/mtg/multiverseid/0.jpg") {
                     card.img_url = card.editions[k].image_url;
+                    imageFound = true;
                     break;
                 }
             }
-            cards[i] = card;
+            if (imageFound) {
+                filteredCards.push(card);
+            }
         }
-        return cards;
+        return filteredCards;
     }
 
 }
