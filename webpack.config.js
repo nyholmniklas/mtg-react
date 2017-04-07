db3625968e077277abebf8deff037c82ecc5f08e
niklas <nyholmniklas@gmail.com>
2017-04-07 19:48:04 +0300

    Added some eslint stuff but not done

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 var webpack = require('webpack');
 
 module.exports = {
-	entry: "./js/OwlbrewApp.js",
-	plugins: [
-		new webpack.LoaderOptionsPlugin({
-				debug: true
-			})
-	],
-	devtool: '#eval-source-map',
-	output: {
-		path: __dirname,
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-		    { test: /\.css$/, loaders: ['style', 'css'] },
-			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-		]
-	}
+    entry: "./js/OwlbrewApp.js",
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ],
+    devtool: '#eval-source-map',
+    output: {
+        path: __dirname,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loaders: ['style', 'css']},
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/}
+        ],
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/, // do not lint third-party code
+                loader: 'jshint-loader'
+            }
+        ]
+    }
 }
