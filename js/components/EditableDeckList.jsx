a475835a08bffb0086a0d2706adb2c164a43ffbc
niklas <nyholmniklas@gmail.com>
2017-04-12 19:37:47 +0300

    Fixed carriage position issue in messy way

--- js/components/EditableDeckList.jsx
+++ js/components/EditableDeckList.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
 import { TextArea, Form } from 'semantic-ui-react';
 
+/**
+ * Stateful input. This component is a mess mostly because of a problem
+ * with carriage position when updating in a controlled way.
+ */
 export default class EditableDeckList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            value: ''
+        };
         this.onChange = this.onChange.bind(this);
     }
 
     onChange(event, data) {
-        event.target.selectionEnd;
+        window.clearTimeout(this._timer);
+        this.setState({
+            value: data.value
+        });
         this.props.deckListTextChanged(data.value);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this._timer) {
+            window.clearTimeout(this._timer);
+            this._timer = null;
+        }
+        this._timer = window.setTimeout(function () {
+            if (this.state.value !== nextProps.deckListAsText) {
+                this.setState({value: nextProps.deckListAsText});
+            }
+        }.bind(this), 100);
     }
 
     render() {
         return (
             <Form className="deckForm">
-                <TextArea value={this.props.deckListAsText} className="deckTextArea" onChange={this.onChange}/>
+                <TextArea value={this.state.value} className="deckTextArea" onChange={this.onChange}/>
             </Form>
         );
     }
