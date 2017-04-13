496ef099da7f4d146665cb50633d13de275bcc87
niklas <nyholmniklas@gmail.com>
2017-04-13 19:49:15 +0300

    Added SearchForm

--- js/components/SearchBar.jsx
+++ js/components/SearchBar.jsx
@@ -1,61 +1,13 @@
 import React from 'react';
-import update from 'react-addons-update';
-import { Form, Divider, Header, Icon, Container } from 'semantic-ui-react';
 
-import SearchField from '~/components/SearchField.jsx';
-import LegalitySelector from '~/components/LegalitySelector.jsx';
+import { Divider, Header, Icon, Container } from 'semantic-ui-react';
+
+import SearchForm from '~/components/SearchForm.jsx';
+
+
 
 export default class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = this.props.searchParams;
-        this.handleSearchTextInput = this.handleSearchTextInput.bind(this);
-        this.handleOracleUserInput = this.handleOracleUserInput.bind(this);
-        this.handleSubtypeUserInput = this.handleSubtypeUserInput.bind(this);
-        this.handleManaParamsInput = this.handleManaParamsInput.bind(this);
-        this.handleFormatLegalityChange = this.handleFormatLegalityChange.bind(this);
-    }
 
-    handleSearchTextInput(searchText) {
-        this.setState(update(this.state, {
-            searchText: {$set: searchText}
-        }), this.searchCards);
-    }
-
-    handleOracleUserInput(oracleSearchText) {
-        this.setState(update(this.state, {
-            searchOracleText: {$set: oracleSearchText}
-        }), this.searchCards);
-    }
-
-    handleSubtypeUserInput(subtypeSearchText) {
-        this.setState(update(this.state, {
-            searchSubtypeText: {$set: subtypeSearchText}
-        }), this.searchCards);
-    }
-
-    handleManaParamsInput(manaParams) {
-        this.setState(update(this.state, {
-            manaParams: {$set: manaParams}
-        }), this.searchCards);
-    }
-
-    handleFormatLegalityChange(formatLegalityFilter) {
-        this.setState(update(this.state, {
-            formatLegalityFilter: {$set: formatLegalityFilter}
-        }), this.searchCards);
-    }
-
-    searchCards() {
-        let searchParams = {
-            searchText: this.state.searchText,
-            searchOracleText: this.state.searchOracleText,
-            searchSubtypeText: this.state.searchSubtypeText,
-            manaParams: this.state.manaParams,
-            formatLegalityFilter: this.state.formatLegalityFilter
-        };
-        this.props.searchCards(searchParams);
-    }
 
     render() {
         return (
@@ -65,35 +17,15 @@ export default class SearchBar extends React.Component {
                     owlbrew.io
                 </Header>
                 <Divider/>
-                <Form>
-
-                    <SearchField label="Card name" placeholder="eg. Lightning Bolt"
-                                 value={this.props.searchParams.searchText}
-                                 onUserInput={this.handleSearchTextInput}/>
-                    <SearchField label="Oracle text" placeholder="eg. Draw a card"
-                                 value={this.props.searchParams.searchOracleText}
-                                 onUserInput={this.handleOracleUserInput}/>
-                    <SearchField label="Subtype" placeholder="eg. Goblin"
-                                 value={this.props.searchParams.searchSubtypeText}
-                                 onUserInput={this.handleSubtypeUserInput}/>
-                    <LegalitySelector value={this.props.searchParams.formatLegalityFilter}
-                                      onUserInput={this.handleFormatLegalityChange}/>
-                    <br/>
-                    {/*<ManaColorSelector manaParams={this.state.manaParams}
-                     manaParamsInputCallback={this.handleManaParamsInput}/>
-                     <br/>
-                     Cards: {DeckUtils.getCardCount(this.props.deck)}
-                     <br/>
-                     <ExportButton onClick={this.props.downloadDeckCallback}/>*/}
-                </Form>
+                <SearchForm searchCards={this.props.searchCards} searchParams={this.props.searchParams}/>
             </Container>
         );
     }
 }
 
 SearchBar.propTypes = {
-    searchCards: React.PropTypes.func.isRequired,
     deck: React.PropTypes.object.isRequired,
     downloadDeckCallback: React.PropTypes.func.isRequired,
+    searchCards: React.PropTypes.func.isRequired,
     searchParams: React.PropTypes.object.isRequired
 };
---
+++ js/components/SearchForm.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Form } from 'semantic-ui-react';
+import update from 'react-addons-update';
+
+import SearchField from '~/components/SearchField.jsx';
+import LegalitySelector from '~/components/LegalitySelector.jsx';
+
+export default class SearchForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = this.props.searchParams;
+        this.handleSearchTextInput = this.handleSearchTextInput.bind(this);
+        this.handleOracleUserInput = this.handleOracleUserInput.bind(this);
+        this.handleSubtypeUserInput = this.handleSubtypeUserInput.bind(this);
+        this.handleManaParamsInput = this.handleManaParamsInput.bind(this);
+        this.handleFormatLegalityChange = this.handleFormatLegalityChange.bind(this);
+    }
+
+    handleSearchTextInput(searchText) {
+        this.setState(update(this.state, {
+            searchText: {$set: searchText}
+        }), this.searchCards);
+    }
+
+    handleOracleUserInput(oracleSearchText) {
+        this.setState(update(this.state, {
+            searchOracleText: {$set: oracleSearchText}
+        }), this.searchCards);
+    }
+
+    handleSubtypeUserInput(subtypeSearchText) {
+        this.setState(update(this.state, {
+            searchSubtypeText: {$set: subtypeSearchText}
+        }), this.searchCards);
+    }
+
+    handleManaParamsInput(manaParams) {
+        this.setState(update(this.state, {
+            manaParams: {$set: manaParams}
+        }), this.searchCards);
+    }
+
+    handleFormatLegalityChange(formatLegalityFilter) {
+        this.setState(update(this.state, {
+            formatLegalityFilter: {$set: formatLegalityFilter}
+        }), this.searchCards);
+    }
+
+    searchCards() {
+        let searchParams = {
+            searchText: this.state.searchText,
+            searchOracleText: this.state.searchOracleText,
+            searchSubtypeText: this.state.searchSubtypeText,
+            manaParams: this.state.manaParams,
+            formatLegalityFilter: this.state.formatLegalityFilter
+        };
+        this.props.searchCards(searchParams);
+    }
+
+    render() {
+        return (
+            <Form>
+
+                <SearchField label="Card name" placeholder="eg. Lightning Bolt"
+                             value={this.props.searchParams.searchText}
+                             onUserInput={this.handleSearchTextInput}/>
+                <SearchField label="Oracle text" placeholder="eg. Draw a card"
+                             value={this.props.searchParams.searchOracleText}
+                             onUserInput={this.handleOracleUserInput}/>
+                <SearchField label="Subtype" placeholder="eg. Goblin"
+                             value={this.props.searchParams.searchSubtypeText}
+                             onUserInput={this.handleSubtypeUserInput}/>
+                <LegalitySelector value={this.props.searchParams.formatLegalityFilter}
+                                  onUserInput={this.handleFormatLegalityChange}/>
+                <br/>
+                {/*<ManaColorSelector manaParams={this.state.manaParams}
+                 manaParamsInputCallback={this.handleManaParamsInput}/>
+                 <br/>
+                 Cards: {DeckUtils.getCardCount(this.props.deck)}
+                 <br/>
+                 <ExportButton onClick={this.props.downloadDeckCallback}/>*/}
+            </Form>
+        );
+    }
+}
+
+SearchForm.propTypes = {
+    searchCards: React.PropTypes.func.isRequired,
+    searchParams: React.PropTypes.object.isRequired
+};
