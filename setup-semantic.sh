67a5b84ab3f6ca64e608b619f4bb1d7255e9452a
niklas <nyholmniklas@gmail.com>
2017-04-16 00:45:56 +0300

    or not

--- setup-semantic.sh
+++ setup-semantic.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 npm install -g gulp
-mkdir ~/public
-cd ~/public
+cd "$(dirname "$0")"
+cd public
 npm install semantic-ui
 for entry in "$search_dir"/*
 do
   echo "$entry"
 done
-cd ~/public/semantic
+cd semantic
 gulp build
