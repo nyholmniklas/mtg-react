b7a3473c56381876a9e6f56ce47cfc442192e515
niklas <nyholmniklas@gmail.com>
2017-04-16 01:29:56 +0300

    Globally?

--- setup-semantic.sh
+++ setup-semantic.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 cd /app
 cd public
-npm install inquirer@3.0.6
 npm install semantic-ui
 npm install
 cd semantic
+npm install inquirer@3.0.6 -g
 npm install -g gulp
 gulp build
