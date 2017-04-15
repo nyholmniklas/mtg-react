57fa5faaf101caf76b456d76f09afd689343642a
niklas <nyholmniklas@gmail.com>
2017-04-16 01:40:13 +0300

    testing out now

--- setup-semantic.sh
+++ setup-semantic.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 cd ~/public
 npm install semantic-ui
-cd ~/public/
+ls
+cd /public
+cd /js
+cd js
+cd ~/js
 cd ~/public/semantic
 npm install inquirer@3.0.6
 npm install -g gulp
