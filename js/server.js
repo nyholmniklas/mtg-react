c66b8c986a200c254909ea35bb14e56820e4f727
niklas <nyholmniklas@gmail.com>
2017-04-19 00:26:22 +0300

    Formatting

--- js/server.js
+++ js/server.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 
-
 const app = express();
 const indexPath = path.join(__dirname, '/../public/index.html');
 const publicPath = express.static(path.join(__dirname, '../public'));
@@ -12,6 +11,5 @@ app.get('/', function (_, res) {
 });
 
 const port = (process.env.PORT || 8080);
-
 app.listen(port);
 console.log(`Listening at http://localhost:${port}`);
