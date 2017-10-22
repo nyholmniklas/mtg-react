aba852488f7343fcc21399301e9e04087ef96653
niklas <nyholmniklas@gmail.com>
2017-10-22 03:28:47 +0300

    Format legality filter works again

--- js/libs/deckBrewApi.js
+++ js/libs/deckBrewApi.js
@@ -2,7 +2,6 @@ import * as HttpResponse2Model from '~/libs/httpResponse2model.js';
 import * as QueryBuilder from '~/libs/queryBuilder.js';
 
 class DeckBrewApi {
-
     static getCards(searchParams) {
         let searchText = searchParams.searchText;
         let searchOracleText = searchParams.searchOracleText;
@@ -11,49 +10,77 @@ class DeckBrewApi {
         let manaParams = searchParams.manaParams;
         let sets = [];
         let formatLegalityFilter = searchParams.formatLegalityFilter;
-        return new Promise(
-            function (resolve) {
-                // Do not even make request if search text is only param and it is shorter than three characters
-                if (searchText.length < 3 && searchOracleText === '' && searchTypeText === '' && searchSubtypeText === '' && manaParams === '' && sets === '') {
-                    resolve();
-                    return;
-                }
-                var xmlHttp = new XMLHttpRequest();
+        return new Promise(function(resolve) {
+            // Do not even make request if search text is only param and it is shorter than three characters
+            if (
+                searchText.length < 3 &&
+                searchOracleText === '' &&
+                searchTypeText === '' &&
+                searchSubtypeText === '' &&
+                manaParams === '' &&
+                sets === ''
+            ) {
+                resolve();
+                return;
+            }
+            var xmlHttp = new XMLHttpRequest();
 
-                xmlHttp.onreadystatechange = function () {
-                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) resolve([HttpResponse2Model.getCardsFromResponse(xmlHttp), searchParams]);
-                    else if (xmlHttp.readyState == 4 && xmlHttp.status != 200) resolve();
-                };
-                // let requestUrlParams = QueryBuilder.buildQueryParamsForFuzzyRequest(searchText, searchOracleText,
-                //     searchTypeText, searchSubtypeText, manaParams, sets, formatLegalityFilter);
-                let requestUrlParams = searchText;
-                xmlHttp.open('GET', 'https://api.scryfall.com/cards/search?q=' + requestUrlParams);
-                xmlHttp.send(null);
-            });
+            xmlHttp.onreadystatechange = function() {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    resolve([
+                        HttpResponse2Model.getCardsFromResponse(xmlHttp),
+                        searchParams
+                    ]);
+                else if (xmlHttp.readyState == 4 && xmlHttp.status != 200)
+                    resolve();
+            };
+            let requestUrlParams = QueryBuilder.buildQueryParamsForFuzzyRequest(
+                searchText,
+                searchOracleText,
+                searchTypeText,
+                searchSubtypeText,
+                manaParams,
+                sets,
+                formatLegalityFilter
+            );
+            xmlHttp.open(
+                'GET',
+                'https://api.scryfall.com/cards/search?q=' + requestUrlParams
+            );
+            xmlHttp.send(null);
+        });
     }
 
     static getCard(cardName) {
-        return new Promise(
-            function (resolve) {
-                // Do not even make request if search text is only param and it is shorter than 2 characters
-                if (cardName.length < 2) {
-                    resolve();
-                    return;
+        return new Promise(function(resolve) {
+            // Do not even make request if search text is only param and it is shorter than 2 characters
+            if (cardName.length < 2) {
+                resolve();
+                return;
+            }
+            var xmlHttp = new XMLHttpRequest();
+            xmlHttp.onreadystatechange = function() {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                    resolve(HttpResponse2Model.getCardFromResponse(xmlHttp));
+                else if (xmlHttp.readyState == 4 && xmlHttp.status != 200) {
+                    resolve({});
                 }
-                var xmlHttp = new XMLHttpRequest();
-                xmlHttp.onreadystatechange = function () {
-                    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) resolve(HttpResponse2Model.getCardFromResponse(xmlHttp));
-                    else if (xmlHttp.readyState == 4 && xmlHttp.status != 200) {
-                        resolve({});
-                    }
-                };
-                cardName = cardName.replace(',', '-').replace(/\s/g, '-').replace('\'', '').replace('--', '-').toLowerCase();
-                xmlHttp.open('GET', 'https://api.scryfall.com/cards/named?exact=' + cardName);
-                xmlHttp.onerror = function () {
-                    console.log('error' + xmlHttp.status);
-                };
-                xmlHttp.send(null);
-            });
+            };
+            cardName = cardName
+                .replace(',', '-')
+                .replace(/\s/g, '-')
+                .replace('\'', '')
+                .replace('--', '-')
+                .toLowerCase();
+            xmlHttp.open(
+                'GET',
+                'https://api.scryfall.com/cards/named?exact=' + cardName
+            );
+            xmlHttp.onerror = function() {
+                console.log('error' + xmlHttp.status);
+            };
+            xmlHttp.send(null);
+        });
     }
 }
 
--- js/libs/queryBuilder.js
+++ js/libs/queryBuilder.js
@@ -1,43 +1,73 @@
-export function buildQueryParamsForFuzzyRequest(searchText, searchOracleText, searchTypeText, searchSubtypeText, manaParams, sets, formatLegalityFilter) {
+export function buildQueryParamsForFuzzyRequest(
+    searchText,
+    searchOracleText,
+    searchTypeText,
+    searchSubtypeText,
+    manaParams,
+    sets,
+    formatLegalityFilter
+) {
     //Set query params
-    let setQuery = '';
-    for (var i = 0; i < sets.length; i++) {
-        setQuery = setQuery + '&set=' + sets[i];
-    }
-    setQuery = '';
-    //Type query params
-    let typeQuery = '';
-    if (searchTypeText != null && searchTypeText != '') {
-        typeQuery = '&type=' + searchTypeText;
-    }
-    //Subtype query params
-    let subtypeQuery = '';
-    if (searchSubtypeText != null && searchSubtypeText != '') {
-        subtypeQuery = '&subtype=' + searchSubtypeText;
-    }
-    //Mana colors query params
-    let manaQuery = '';
-    if (manaParams.white || manaParams.blue || manaParams.black || manaParams.red || manaParams.green) {
-        if (manaParams.white) {
-            manaQuery = manaQuery.concat('&color=white');
-        }
-        if (manaParams.blue) {
-            manaQuery = manaQuery.concat('&color=blue');
-        }
-        if (manaParams.black) {
-            manaQuery = manaQuery.concat('&color=black');
-        }
-        if (manaParams.red) {
-            manaQuery = manaQuery.concat('&color=red');
-        }
-        if (manaParams.green) {
-            manaQuery = manaQuery.concat('&color=green');
-        }
-    }
-    if (searchOracleText == null) searchOracleText = '';
-    let formatFilter = '';
-    if (formatLegalityFilter !== '') formatFilter = '&format=' + formatLegalityFilter;
-    const requestUrlParams = 'name=' + searchText + '&oracle=' + searchOracleText
-        + typeQuery + setQuery + subtypeQuery + manaQuery + formatFilter;
+    // let setQuery = '';
+    // setQuery = MyTrim(setQuery);
+    // for (var i = 0; i < sets.length; i++) {
+    //     setQuery = setQuery + '&set=' + sets[i];
+    // }
+    // setQuery = '';
+    // //Type query params
+    // let typeQuery = '';
+    // if (searchTypeText != null && searchTypeText != '') {
+    //     typeQuery = '&type=' + searchTypeText;
+    // }
+    // //Subtype query params
+    // let subtypeQuery = '';
+    // if (searchSubtypeText != null && searchSubtypeText != '') {
+    //     subtypeQuery = '&subtype=' + searchSubtypeText;
+    // }
+    // //Mana colors query params
+    // let manaQuery = '';
+    // if (
+    //     manaParams.white ||
+    //     manaParams.blue ||
+    //     manaParams.black ||
+    //     manaParams.red ||
+    //     manaParams.green
+    // ) {
+    //     if (manaParams.white) {
+    //         manaQuery = manaQuery.concat('&color=white');
+    //     }
+    //     if (manaParams.blue) {
+    //         manaQuery = manaQuery.concat('&color=blue');
+    //     }
+    //     if (manaParams.black) {
+    //         manaQuery = manaQuery.concat('&color=black');
+    //     }
+    //     if (manaParams.red) {
+    //         manaQuery = manaQuery.concat('&color=red');
+    //     }
+    //     if (manaParams.green) {
+    //         manaQuery = manaQuery.concat('&color=green');
+    //     }
+    // }
+    // if (searchOracleText == null) searchOracleText = '';
+    // let formatFilter = '';
+    // if (formatLegalityFilter !== '')
+    //     formatFilter = '&format=' + formatLegalityFilter;
+    // const requestUrlParams =
+    //     'name=' +
+    //     searchText +
+    //     '&oracle=' +
+    //     searchOracleText +
+    //     typeQuery +
+    //     setQuery +
+    //     subtypeQuery +
+    //     manaQuery +
+    //     formatFilter;
+    const requestUrlParams = setFormat(searchText, formatLegalityFilter);
     return requestUrlParams;
 }
+
+let setFormat = (query, format) => {
+    if (format) query = query + '+f=' + format;
+    return query;
+};
